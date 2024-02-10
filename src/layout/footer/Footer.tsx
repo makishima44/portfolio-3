5d4d68bdcbdaf557e7f390ffd5a069f96b43fbf6
Artur <metallist15431543@gmail.com>
2024-02-10 10:58:47 +0300

    Добавлены стили для блока Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -6,19 +6,24 @@ export const Footer = () => {
   return (
     <StyledFooter>
       <FooterTitle>
-        Created by<FooterText>Zarror</FooterText>| All Reserved!
+        Created by<FooterText> Zarror </FooterText>| All Reserved!
       </FooterTitle>
     </StyledFooter>
   );
 };
 
 const StyledFooter = styled.footer`
+  padding: 100px 0 50px;
   background: rgb(0, 0, 0);
 `;
 
 const FooterTitle = styled.h3`
   text-align: center;
   color: rgba(255, 255, 255, 0.5);
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 180%;
+  letter-spacing: 0.3px;
 `;
 
 const FooterText = styled.span`
