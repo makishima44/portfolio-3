56270fc765cda4c74c455155eaa965e17728c8f1
Artur <metallist15431543@gmail.com>
2024-02-12 18:15:46 +0300

    Добавлен адаптив блока Skills

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -9,13 +9,13 @@ export const Skills = () => {
     <StyledSkills>
       <Contrainer>
         {" "}
-        <FlexWrapper justify="space-around" align="center" wrap="wrap">
+        <SkillsWrapper>
           <Icon iconId="figma" height="83" width="55" viewBox="0 0 55 83" />
           <Icon iconId="html" height="81" width="72" viewBox="0 0 72 81" />
           <Icon iconId="css" height="74" width="65" viewBox="0 0 65 74" />
           <Icon iconId="sass" height="75" width="100" viewBox="0 0 100 75" />
           <Icon iconId="react" height="89" width="100" viewBox="0 0 100 93" />
-        </FlexWrapper>
+        </SkillsWrapper>
       </Contrainer>
     </StyledSkills>
   );
@@ -24,4 +24,16 @@ export const Skills = () => {
 const StyledSkills = styled.section`
   background: rgb(0, 0, 0);
   padding: 20px;
+`;
+
+const SkillsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+
+  @media (max-width: 550px) {
+    flex-grow: 1;
+    gap: 20px;
+  }
 `;
